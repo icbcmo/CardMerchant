d91180b96999090b60df5b6c9d824ea7161058ff
pzhu1 <13311098817@189.cn>
2018-08-16 11:59:58 +0800

    完善照片全屏显示页

--- src/pages/home/refund/showimage.ts
+++ src/pages/home/refund/showimage.ts
@@ -1,56 +1,24 @@
 
 import {Component, OnInit} from '@angular/core';
-import { ModalController, Platform,NavController, ViewController } from 'ionic-angular';
-import {CardMerchantService} from "../../../service/card-merchant.service";
-import {NativeStorage} from "@ionic-native/native-storage";
-import { AlertController, LoadingController } from 'ionic-angular';
-import {TipService} from '../../../service/tip.service';
+import { ViewController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-showimage',
   templateUrl: 'showimage.html'
 })
 export class ShowImage implements OnInit{
-	items: any;
+	picurl: any;
 	
     constructor(
         public viewCtrl: ViewController,
-		public modalCtrl: ModalController,
-        public navCtrl: NavController,
-		private nativeStorage: NativeStorage,
-		public loadingCtrl: LoadingController,
-		private alertCtrl: AlertController,
-		public tipService: TipService,
-		public cardMerchantService: CardMerchantService
+		public params: NavParams
     ) {
     }
 
 
     ngOnInit() {
-		
-		var data = {
-			sessionid: localStorage.getItem('SESSIONID'),
-			field1: 1  //1-退款列表 2-机器问题列表
-		};
-		console.log(data);
-		let loading = this.loadingCtrl.create({
-				content: 'Please wait...'
-			});
-		loading.present();
-		this.cardMerchantService.getrequestlist(data).toPromise().then(data=> {
-			console.log(Object(data));
-			loading.dismiss();
-			if(Object(data).code == 0){
-				this.items = Object(data).data;
-			}else{
-				this.alertCtrl.create({
-						message: Object(data).message,
-						buttons: ['确定']
-					}).present();
-			}
-			this.items = [ {datetime: '2018-08-15',currpeople: 'dwkdjwsd'},{datetime: '2018-08-15',currpeople: 'dwkdjwsd'}];
-			console.log(this);
-		});
+		this.picurl = this.params.get('picurl');
+		this.picurl = './assets/imgs/juana.gif';
 	}
 
     dismiss() {
