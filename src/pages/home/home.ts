d0f52176a41bceb27fe0a035ad4a978a9a1573b4
Vincent <fly9879@263.net>
2018-08-21 11:27:23 +0800

    icon & splash 圖片修改

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -257,22 +257,30 @@ export class HomePage implements OnInit{
     }
 	
 	openScanner(){
-        const options: CameraOptions = {
-            quality: 80,
-            targetWidth: 600,
-            targetHeight: 1200,
-            //allowEdit: true,
-            sourceType: 1,
-            destinationType: this.camera.DestinationType.DATA_URL,
-            encodingType: this.camera.EncodingType.JPEG,
-            mediaType: this.camera.MediaType.PICTURE
-        };
+        // Optionally request the permission early
+        this.qrScanner.prepare()
+            .then((status: QRScannerStatus) => {
+                if (status.authorized) {
+                    // camera permission was granted
 
-        this.camera.getPicture(options).then((imageData) => {
-            let base64Image = imageData;
-            base64Image = 'data:image/jpeg;base64,' + base64Image;
-            console.log(base64Image);
-        });
+
+                    // start scanning
+                    let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+                        console.log('Scanned something', text);
+
+                        this.qrScanner.hide(); // hide camera preview
+                        scanSub.unsubscribe(); // stop scanning
+                    });
+
+                } else if (status.denied) {
+                    // camera permission was permanently denied
+                    // you must use QRScanner.openSettings() method to guide the user to the settings page
+                    // then they can grant the permission from there
+                } else {
+                    // permission was denied, but not permanently. You can ask for permission again at a later time.
+                }
+            })
+            .catch((e: any) => console.log('Error is', e));
     }
 
     openRefundModal() {
