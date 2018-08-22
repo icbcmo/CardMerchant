81178f8a5601261196a49c02b50b52c090067f68
Vincent <fly9879@263.net>
2018-08-22 17:14:32 +0800

    新增幫助畫面

---
+++ src/pages/home/help/help.ts
@@ -0,0 +1,30 @@
+
+import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import { ViewController, AlertController, LoadingController } from 'ionic-angular';
+import {TipService} from '../../../service/tip.service';
+import {CardMerchantService} from "../../../service/card-merchant.service";
+
+@Component({
+  selector: 'page-wrongtrx',
+  templateUrl: 'help.html'
+})
+export class Help implements OnInit{
+	@ViewChild('billno') Billno: ElementRef;
+
+    constructor(
+        public viewCtrl: ViewController,
+		public tipService: TipService,
+		private alertCtrl: AlertController,
+		public loadingCtrl: LoadingController,
+		public cardMerchantService: CardMerchantService
+    ) {
+    }
+
+
+    ngOnInit() {}
+
+    goBack(){
+        this.viewCtrl.dismiss();
+    }
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,7 +16,7 @@ import {Qrscanner} from "./qrscanner/qrscanner";
 import {Qrcode} from "./qrcode/qrcode";
 import { JPush } from "@jiguang-ionic/jpush";
 import { Device } from "@ionic-native/device";
-import {Setup} from "../contact/setup/setup";
+import {Help} from "./help/help";
 import {Machine} from "./machine/machine";
 import {CashierScan} from "./cashier/scan/cashier-scan";
 import {OrderList} from "./order/orderlist";
@@ -324,7 +324,7 @@ export class HomePage implements OnInit{
     }
 	
 	goSetup() {
-        let modal = this.modalCtrl.create(Setup);
+        let modal = this.modalCtrl.create(Help);
         modal.present();
     }
 	
